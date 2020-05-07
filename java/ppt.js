b18b6edb39ae4f84cc9ff318d3ec8db0b5e834a5
Marco Aurelio <marco18ovi@gmail.com>
2020-05-06 20:20:47 -0500

    se añadio un js

---
+++ java/ppt.js
@@ -0,0 +1,63 @@
+function aleatorio(minimo, maximo)
+{
+    var numero = Math.floor( Math.random() * (maximo - minimo +1) + minimo);
+    return numero;
+}
+
+var piedra = 0;
+var papel = 1;
+var tijera = 2;
+var opciones = ["piedra", "papel", "tijera"];
+
+// alert( opciones[1]);
+
+var opcionusuario;
+var opcionmaquina = aleatorio(0,2);
+
+opcionusuario = prompt("¿que eliges?\nPiedra: 0\nPapel: 1\nTijera:2",0);
+
+// alert( "elegiste " + opcionusuario);
+
+alert("Elegiste " + opciones[opcionusuario]);
+alert("javascript eligio " + opciones[opcionmaquina]);
+
+if(opcionusuario == piedra ) {
+    // alert("elegiste piedra");
+    if(opcionmaquina == piedra){
+        alert("empate 😱");
+    }
+    else if(opcionmaquina == papel){
+        alert("perdiste 😭");
+    }
+    else if(opcionmaquina == tijera){
+        alert("ganaste 🤩");
+    }
+}
+else if(opcionusuario == papel) {
+    // alert("elegiste papel");
+    if(opcionmaquina == piedra){
+        alert("ganaste 🤩");
+    }
+    else if(opcionmaquina == papel){
+        alert("empate 😱");
+    }
+    else if(opcionmaquina == tijera){
+        alert("perdiste 😭");
+    }
+}
+else if(opcionusuario == tijera){
+    // alert("elegiste tijera");
+    if(opcionmaquina == piedra){
+        alert("perdiste 😭");
+    }
+    else if(opcionmaquina == papel){
+        alert("ganaste 🤩");
+    }
+    else if(opcionmaquina == tijera){
+        alert("empate 😱");
+    }
+}
+else {
+    alert("elige bien sino te pego 😠")
+}
+
